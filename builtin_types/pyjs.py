13ac4545cb94958db80bfa5fcc9ff28192a9e7c9
PiotrDabkowski <piodrus@gmail.com>
2014-10-10 17:11:56 +0100

    test pycharm

--- builtin_types/pyjs.py
+++ builtin_types/pyjs.py
@@ -1,3 +1,15 @@
 class PyJs:
     '''Base PyJs object! Father of all PyJs objects'''
     pass
+
+class Megan:
+    def __init__(self):
+        pass
+
+    def kupal(self):
+        """
+        """
+
+    def __mul__(self, other):
+        pass
+
