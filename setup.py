f7389355acecb08ba1b03ccbf951df008aae4522
Piotr Dabkowski <piodrus@gmail.com>
2015-08-22 10:25:56 +0200

    Fixed readme...

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
 
+
 setup(
     name='Js2Py',
-    version='0.26',
+    version='0.27',
     packages=['js2py', 'js2py.utils', 'js2py.prototypes', 'js2py.translators', 'js2py.constructors', 'js2py.host'],
     url='https://github.com/PiotrDabkowski/Js2Py',
     install_requires = ['tzlocal>=1.2'],
     license='GPL',
     author='Piotr Dabkowski',
     author_email='piotr.dabkowski@balliol.ox.ac.uk',
-    description='JavaScript to Python translator'
+    description='JavaScript to Python Translator & JavaScript interpreter written in 100% pure Python.',
+    long_description=open('README.rst', 'rt').read()
 )
