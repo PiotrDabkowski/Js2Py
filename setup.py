a31d3a9c7bdaf6f0ed847df88b63d8dab3e27bcf
Samuel Bishop <techdragon@Sams-MacBook-Air.local>
2015-08-27 14:07:21 +0800

    License information improvements
    - Corrected setup.py license information to match the included license file.
    - Capitalised license filename.
    - Changed license file extension to markdown to match the README file.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=['js2py', 'js2py.utils', 'js2py.prototypes', 'js2py.translators', 'js2py.constructors', 'js2py.host'],
     url='https://github.com/PiotrDabkowski/Js2Py',
     install_requires = ['tzlocal>=1.2'],
-    license='GPL',
+    license='MIT',
     author='Piotr Dabkowski',
     author_email='piotr.dabkowski@balliol.ox.ac.uk',
     description='JavaScript to Python Translator & JavaScript interpreter written in 100% pure Python.',
