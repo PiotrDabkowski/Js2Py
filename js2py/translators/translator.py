06f44e1f4f186b4d237a34bf9519f9aafcb691ab
Robert <lexcon@bign.nl>
2016-04-07 00:45:54 +0200

    Disabled compilation plans when there are // or /* tags to avoid regex-parse errors of constants

--- js2py/translators/translator.py
+++ js2py/translators/translator.py
@@ -56,7 +56,7 @@ def dbg(x):
 def translate_js(js, HEADER=DEFAULT_HEADER, use_compilation_plan=False):
     """js has to be a javascript source code.
        returns equivalent python code."""
-    if use_compilation_plan:
+    if use_compilation_plan and not '//' in js and not '/*' in js:
         return translate_js_with_compilation_plan(js, HEADER=HEADER)
     parser = pyjsparser.PyJsParser()
     parsed = parser.parse(js) # js to esprima syntax tree
